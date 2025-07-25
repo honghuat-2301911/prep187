f9fc38507f3b94fd7a31cadec524b4e4bb42eee8
Hong Huat <158584424+honghuat-2301911@users.noreply.github.com>
2025-07-25 06:10:32 +0000

    qwe

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,21 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginSecurity from 'eslint-plugin-security';
-import pluginNoUnsanitized from 'eslint-plugin-no-unsanitized';
-
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginSecurity from "eslint-plugin-security";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  pluginJs.configs.recommended,
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      globals: {
-        ...globals.browser
-      }
-    },
-    plugins: {
-      security: pluginSecurity,
-      'no-unsanitized': pluginNoUnsanitized
-    },
-    rules: {
-      'security/detect-eval-with-expression': 'error',
-      'no-console': 'warn'
-    }
-  },
-  {
-    files: ['tests/**/*.js'],
-    languageOptions: {
-      globals: {
-        ...globals.mocha
-      }
-    }
-  }
+{files: ["**/*.{js,mjs,cjs,jsx}"]},
+{languageOptions: { globals: globals.browser }},
+pluginJs.configs.recommended,
+pluginReact.configs.flat.recommended,
+{
+plugins: {
+security: pluginSecurity
+},
+rules: {
+...pluginJs.configs.recommended.rules,
+...pluginReact.configs.flat.recommended.rules,
+"security/detect-eval-with-expression": "error",
+}
+}
 ];
